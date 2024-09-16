5dc03a14791cad1988edd9ae476e4d02b0c7fc4b
James Bartlett <james.bartlett@glasgow.ac.uk>
2024-09-16 22:13:07 +0100

    Initial ODL edits for deadlines

--- include/deadlines.R
+++ include/deadlines.R
@@ -1,17 +1,17 @@
 study_year <- "2024 / 2025"
 
-groupagreement_due <- "October 11th 2024"
+groupagreement_due <- "March 14th 2025"
 
-stage1_due <- "November 15th 2024"
+stage1_due <- "April 23rd 2025"
 
 stage1_words <- 1500
 
-stage2_due <- "December 13th 2024"
+stage2_due <- "June 11th 2025"
 
 stage2_words <- 1500
 
-MCQ_due <- "October 25th 2024"
+MCQ_due <- "November 13th 2024"
 
-dataskills1_due <- "November 8th 2024"
+dataskills1_due <- "December 4th 2024"
 
-dataskills2_due <- "November 29th 2024"
+dataskills2_due <- "January 22nd 2025"
